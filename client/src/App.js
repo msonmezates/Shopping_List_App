641a82454a6ba4952b5ce47918052afb2de6bed8
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2018-07-05 13:26:31 -0500

    Add dependencies for reactstrap and create a navbar, render inside the main file

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import AppNavbar from './components/AppNavbar';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h2>Client</h2>
+        <AppNavbar />
       </div>
     );
   }
