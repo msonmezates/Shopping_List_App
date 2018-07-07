4f97628a4d76771e9970c731723ee54142028f97
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2018-07-06 20:33:04 -0500

    Prepare for deployment

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const items = require('./routes/api/items');
 
@@ -19,5 +20,15 @@ app.use('/api/items', items);
 mongoose.connect(db)
   .then(() => console.log('MongoDB connected...'))
   .catch(err => console.log(err));
+
+// Serve static assets if in production
+if(process.env.NODE_ENV === 'production') {
+  // set the static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')); // it's directing from current directory to index.html in client 
+  });                                                                       // when it's in production
+}
 
 app.listen(process.env.PORT || 5000, () => console.log('Server has started!'));
