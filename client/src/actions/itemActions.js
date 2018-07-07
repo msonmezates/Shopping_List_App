67bf6b785a1309d55b57435d39d7036c83db1165
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2018-07-06 19:02:59 -0500

    Delete functionality is connected to database so that it removes the item from db when it's clicked

--- client/src/actions/itemActions.js
+++ client/src/actions/itemActions.js
@@ -18,11 +18,12 @@ export const addItem = newItem => dispatch => {
     }))
 }
 
-export const deleteItem = id => {
-  return {
-    type: DELETE_ITEM,
-    payload: id
-  };
+export const deleteItem = id => dispatch  => {
+  axios.delete(`/api/items/${id}`)
+    .then(res => dispatch({
+      type: DELETE_ITEM,
+      payload: id
+    }))
 }
 
 export const setItemsLoading = () => {
